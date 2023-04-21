37869d55ee497476bb037c46aeaeb53d4dd34593
tyler-radke1 <109247761+tyler-radke1@users.noreply.github.com>
2023-04-21 11:08:31 -0600

    calendar view is now fucntional, table view reads the date from the calendar

--- Meal Planner/Calendars/CalendarView.swift
+++ Meal Planner/Calendars/CalendarView.swift
@@ -10,7 +10,12 @@ import UIKit
 class CalendarView: UIViewController, UICalendarSelectionSingleDateDelegate, UITableViewDelegate, UITableViewDataSource, UICalendarViewDelegate {
     
     
+    var days: [Date: Day] = [:]
     
+    private var loadedDate = Date.now
+    private var loadedBreakfast: Recipe?
+    private var loadedLunch: Recipe?
+    private var loadedDinner: Recipe?
     
     @IBOutlet weak var calendarTableView: UITableView!
     
@@ -21,7 +26,10 @@ class CalendarView: UIViewController, UICalendarSelectionSingleDateDelegate, UIT
         
         calendarTableView.dataSource = self
         calendarTableView.delegate = self
-     
+    }
+    
+    func generateFood(for day: Date) {
+        
     }
     
     func configure(calendar: UICalendarView) {
@@ -43,7 +51,22 @@ class CalendarView: UIViewController, UICalendarSelectionSingleDateDelegate, UIT
     }
     
     func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
-       // guard let dateComponents else { return }
+        guard let dateComponents, let date = dateComponents.date else { return }
+        loadedDate = date
+        let day = days[date]
+        
+        if let breakfast = day?.breakfast {
+            loadedBreakfast = breakfast
+        }
+        
+        if let lunch = day?.lunch {
+            loadedLunch = lunch
+        }
+        
+        if let dinner = day?.dinner {
+            loadedDinner = dinner
+        }
+        calendarTableView.reloadData()
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -56,11 +79,32 @@ class CalendarView: UIViewController, UICalendarSelectionSingleDateDelegate, UIT
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = calendarTableView.dequeueReusableCell(withIdentifier: "mealCell", for: indexPath) as! MealCell
+       
+        configure(cell: cell, at: indexPath)
         return cell
+    }
+    func configure(cell: MealCell, at indexPath: IndexPath) {
+        switch indexPath.section {
+        case 0:
+            cell.mealNameLabel.text = "Breakfast"
+        case 1:
+            cell.mealNameLabel.text = "Lunch"
+        case 2:
+            cell.mealNameLabel.text = "Dinner"
+        default:
+            cell.mealNameLabel.text = "Coming"
+        }
+        
+        let formatter = DateFormatter()
+        
+        formatter.dateStyle = .short
+        
+        cell.recipeNameLabel.text = "\(formatter.string(from: loadedDate))"
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
+        
     }
 }
