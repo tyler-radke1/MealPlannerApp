1f38376ca22c86028ec3be03238f8ca6361782e0
LimeFreezy98 <89162212+LimeFreezy98@users.noreply.github.com>
2023-05-02 10:21:56 -0600

    still stuck

--- Meal Planner/Storyboard Screens/Ingredients/IngredientViewController.swift
+++ Meal Planner/Storyboard Screens/Ingredients/IngredientViewController.swift
@@ -107,23 +107,16 @@ class IngredientViewController: UIViewController, UITableViewDataSource, UITable
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         return true
     }
-
+//MARK: i dont like alphibeticle order while i working on moving the rows
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let movedIngredient = ingredients[sourceIndexPath.row]
         ingredients.remove(at: sourceIndexPath.row)
         ingredients.insert(movedIngredient, at: destinationIndexPath.row)
         
-//        for (index, ingredient) in ingredients.enumerated() {
-//               ingredient.order = Int16(index)
-//           }
-        
-//        do {
-//               try context.save()
-//           } catch {
-//               print("Failed to save changes")
-//           }
-//        tableView.reloadData()
+
     }
+       
+       
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ingredientsCell", for: indexPath)  as! IngredientTableViewCell
