172ea7663e20a6c5666473957728e992c9acf25b
ami-smith <111449179+ami-smith@users.noreply.github.com>
2023-05-03 09:25:40 -0600

    saving calendar

--- Meal Planner/Calendars/MealCell.swift
+++ Meal Planner/Calendars/MealCell.swift
@@ -9,7 +9,7 @@ import UIKit
 import CoreData
 
 protocol MealCellDelegate {
-    func updateMeal(for type: MealType, with recipe: Recipe)
+    func updateMeal(for type: MealType, with recipe: Recipe?)
     func saveCoreData(date: Date, day: Day)
 }
 
@@ -23,6 +23,8 @@ class MealCell: UITableViewCell {
     
     var cellMeal: MealType? = nil
     
+    let noMeal: Recipe? = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -34,13 +36,18 @@ class MealCell: UITableViewCell {
         // Configure the view for the selected state
     }
     func addMenuItems()  -> UIMenu  {
+        guard let cellMeal, let delegate else { return UIMenu()}
         var actions: [UIAction] = []
         
+        let noMealAction = UIAction(title: "Leave Blank") { action in
+           delegate.updateMeal(for: cellMeal, with: nil)
+        }
+        actions.append(noMealAction)
+        
         for recipe in createFetchRequest() {
-            guard let delegate, let cellMeal, let name = recipe.name else { continue }
+            guard let name = recipe.name else { continue }
             
             let action = UIAction(title: name) { action in
-                
                 delegate.updateMeal(for: cellMeal, with: recipe)
             }
             actions.append(action)
